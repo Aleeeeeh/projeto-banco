5f487e2afecb016bb83aeb7388c02f0dcf52b375
= <alefe.ferreira@acovisa.com.br>
2023-11-18 09:07:09 -0300

    Composição e encapsulamento

--- contas/contaCorrente.go
+++ contas/contaCorrente.go
@@ -1,39 +1,51 @@
 package contas
 
+import "banco/clientes"
+
+/*
+public -> Começa com letra maiuscula
+private -> Começa com letra minuscula
+Com isso private só é possível acessar de fato a propriedade dentro do mesmo arquivo, conforme
+exemplo da propriedade saldo
+*/
 type ContaCorrente struct {
-	Titular       string
+	Titular       clientes.Titular
 	NumeroAgencia int
 	NumeroConta   int
-	Saldo         float64
+	saldo         float64
 }
 
 // c *ContaCorrente faz o método ser referência da struct ContaCorrente
 func (c *ContaCorrente) Sacar(valorDoSaque float64) string {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.Saldo
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
 
 	if podeSacar {
-		c.Saldo -= valorDoSaque
+		c.saldo -= valorDoSaque
 		return "Saque realizado com sucesso"
 	}
 
-	return "Saldo insuficiente"
+	return "saldo insuficiente"
 }
 
 func (c *ContaCorrente) Depositar(valor float64) (string, float64) {
 	if valor >= 0 {
-		c.Saldo += valor
-		return "Depósito realizado com sucesso, valor de", valor
+		c.saldo += valor
+		return "Depósito realizado com sucesso no valor de", valor
 	}
 
 	return "Valor do depósito menor que zero, valor informador de", valor
 }
 
 func (c *ContaCorrente) Transferir(valor float64, contaDestino *ContaCorrente) bool {
-	if valor < c.Saldo && valor > 0 {
-		c.Saldo -= valor
+	if valor < c.saldo && valor > 0 {
+		c.saldo -= valor
 		contaDestino.Depositar(valor)
 		return true
 	}
 
 	return false
+}
+
+func (c *ContaCorrente) ObterSaldo() float64 {
+	return c.saldo
 }
--- main.go
+++ main.go
@@ -1,30 +1,35 @@
 package main
 
 import (
+	"banco/clientes"
 	"banco/contas"
 	"fmt"
 )
 
 func main() {
-	contaDoGopher := contas.ContaCorrente{Titular: "Gopher"}
+	clienteGopher := clientes.Titular{Nome: "Gopher", Cpf: "123.456.789.10", Profissao: "Dev"}
+	contaDoGopher := contas.ContaCorrente{Titular: clienteGopher, NumeroAgencia: 6882, NumeroConta: 55669}
 
 	var deposito float64
 	fmt.Println("Digite o valor para depósito: ")
 	fmt.Scan(&deposito)
 	status, valor := contaDoGopher.Depositar(deposito)
 	fmt.Println(status, valor)
+	fmt.Println("Saldo em conta do", contaDoGopher.Titular.Nome, ":", contaDoGopher.ObterSaldo())
+	fmt.Println()
 
-	fmt.Println("Saldo em conta do gopher:", contaDoGopher.Saldo)
 	var saque float64
 	fmt.Println("Digite o valor do saque: ")
 	fmt.Scan(&saque)
 	fmt.Println(contaDoGopher.Sacar(saque))
-	fmt.Println("Saldo atualizado da conta do gopher:", contaDoGopher.Saldo)
+	fmt.Println("Saldo atualizado da conta do", contaDoGopher.Titular.Nome, ":", contaDoGopher.ObterSaldo())
+	fmt.Println()
 
-	contaGopherzinho := contas.ContaCorrente{Titular: "Gopherzinho"}
+	clienteGopherzinho := clientes.Titular{Nome: "Gopherzinho", Cpf: "111.222.333.44", Profissao: "devinho"}
+	contaGopherzinho := contas.ContaCorrente{Titular: clienteGopherzinho, NumeroAgencia: 6882, NumeroConta: 5559874}
 
 	var valorTransferencia float64
-	fmt.Println("Insira valor da transferência para conta do gopherzinho: ")
+	fmt.Println("Insira valor da transferência para conta do", contaGopherzinho.Titular.Nome)
 	fmt.Scan(&valorTransferencia)
 	transferencia := contaDoGopher.Transferir(valorTransferencia, &contaGopherzinho)
 	fmt.Println("Resposta da transação:", transferencia)
